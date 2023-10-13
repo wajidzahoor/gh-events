b654910ef055c7dd516a1d017a419f906d293f3e
wajidzahoor <wajidzahoor@gmail.com>
2023-10-13 21:07:12 +0100

    Tests not tests

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-// My tests
+// My Tests
 
 describe('MainContent', () => {
   it('should render a button', () => {
